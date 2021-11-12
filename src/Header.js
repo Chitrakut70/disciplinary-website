098af11dfddf9789581cd9ab67bba481aa0f601c
Chitrakut70 <87463213+Chitrakut70@users.noreply.github.com>
2021-11-12 16:01:04 +0530

    navbar-added

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import'./Header.css';
+function Header() {
+    return (
+        <div className="header-class">
+        <div className="header-name">DISPILINARY COMMITTEE</div>
+        <ul className="header-components">
+            <li>ABOUT</li>
+            <li>COORDINATORS</li>
+            <li>CONTACT US</li>
+        </ul>
+        </div>
+    )
+}
+
+export default Header

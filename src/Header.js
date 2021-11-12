455934f3b04299df6df9be6e6091e40ef2a70bce
Chitrakut70 <87463213+Chitrakut70@users.noreply.github.com>
2021-11-12 18:32:52 +0530

    header paritial responsive

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import'./Header.css';
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 function Header() {
     return (
         <div className="header-class">
-        <div className="header-name">DISPILINARY COMMITTEE</div>
-        <ul className="header-components">
-            <li>ABOUT</li>
-            <li>COORDINATORS</li>
-            <li>CONTACT US</li>
-        </ul>
+            <div className="header-name">DISCIPILINARY COMMITTEE</div>
+            <ul className="header-components">
+                <li><MenuRoundedIcon/></li>
+                <li>ABOUT</li>
+                <li>COORDINATORS</li>
+                <li>CONTACT US</li>
+            </ul>
         </div>
     )
 }
